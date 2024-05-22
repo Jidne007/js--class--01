953a7f06c8a1d3eb161ddc02cc016d9b1b777102
Jidne007 <163674890+Jidne007@users.noreply.github.com>
2024-05-22 17:10:34 +0600

    Add files via upload

---
+++ JS--class-1-aliSir/index.js
@@ -0,0 +1,16 @@
+/* Numeric Variable*/
+
+// var age = 55;
+
+// console.log(age);
+
+// var number = 100;
+// console.log(number);
+
+// var price = 516;
+// console.log(price);
+
+
+/*String Variable*/
+
+var name = '' 
